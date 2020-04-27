6536a8f2728ced3495f3c7331dc8d95278c3a174
Lawrence Adrian De Belen <ltdebelen@gmail.com>
2020-04-27 18:30:19 -0500

    Adding website pages

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import "react-mdl/extra/material.css";
+import "react-mdl/extra/material.js";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
