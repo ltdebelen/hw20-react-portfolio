9594e89115f45d2a5ca73da48f60ec60871c43cf
Lawrence Adrian De Belen <ltdebelen@gmail.com>
2020-04-27 20:50:28 -0500

    Landing Page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import "./App.css";
 import { Layout, Header, Navigation, Drawer, Content } from "react-mdl";
 import Main from "./components/Main";
 import { Link } from "react-router-dom";
@@ -8,7 +9,7 @@ class App extends Component {
     return (
       <div className="demo-big-content">
         <Layout>
-          <Header title="Title" scroll>
+          <Header className="header-color" title="Title" scroll>
             <Navigation>
               <Link to="/resume">Resume</Link>
               <Link to="/aboutme">About Me</Link>
